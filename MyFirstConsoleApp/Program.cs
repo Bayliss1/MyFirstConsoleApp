d3d70c2ace41738efb467546150a7993a3e1decd
Owen <owen.bayliss@sky.com>
2022-05-17 21:58:18 +0100

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
